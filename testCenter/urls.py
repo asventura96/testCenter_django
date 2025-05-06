db409342f64daaf4c14e4ddc3c19342910a569c7
André Ventura <ti@asventura.com.br>
2025-05-05 22:36:06 -0300

    Adiciona URLs para exclusão de registros no arquivo urls.py

--- testCenter/urls.py
+++ testCenter/urls.py
@@ -10,6 +10,8 @@ e as URLs para login e logout.
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path
+from .views import delete_item, home
+
 
 urlpatterns = [
     # URL para a interface Administrativa Django
@@ -21,4 +23,9 @@ urlpatterns = [
     # URLs de Login e Logout do Usuário
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # URLs dos Apps do Projeto
+
+    # Path para exclusão de registros
+    path('delete/<str:model_name>/<int:pk>/', delete_item, name='delete_item'),
 ]
