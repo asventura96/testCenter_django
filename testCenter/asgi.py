3c99f32096fbe53dd2c622ca724b925a03393a41
André Ventura <ti@asventura.com.br>
2025-05-05 22:29:58 -0300

    Remove comentários e código não utilizado do arquivo asgi.py

--- testCenter/asgi.py
+++ testCenter/asgi.py
@@ -1,16 +1,1 @@
-"""
-ASGI config for testCenter_django project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testCenter.settings')
-
-application = get_asgi_application()
+# testCenter/asgi.py
