dbba59a17d533bf0485149e0eb42cd6260a07ec4
André Ventura <ti@asventura.com.br>
2025-05-05 22:30:14 -0300

    Adiciona middleware para redirecionar usuários não autenticados para a página de login

---
+++ testCenter/middleware.py
@@ -0,0 +1,33 @@
+# testCenter/middleware.py
+
+"""
+Middleware para garantir que o usuário esteja autenticado antes de acessar certas URLs.
+"""
+
+from django.conf import settings
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+
+EXEMPT_URLS = [
+    settings.LOGIN_URL.lstrip('/'),
+    'recuperar-senha/',
+    'recuperar-senha/sucesso/',
+    'reset/',
+    'reset/sucesso/',
+    reverse_lazy('login'),
+    reverse_lazy('logout'),
+]
+
+class LoginRequiredMiddleware:
+    """
+    Middleware que redireciona usuários não autenticados para a página de login.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        path = request.path_info.lstrip('/')
+        if not request.user.is_authenticated:
+            if not any(path.startswith(str(url)) for url in EXEMPT_URLS):
+                return redirect(settings.LOGIN_URL)
+        return self.get_response(request)
