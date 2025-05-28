c93585702383b1b91e74cbf41022647cff1c2dc8
André Ventura <ti@asventura.com.br>
2025-05-28 16:01:42 -0300

    Adiciona formulário para cadastro e edição de Certificações com campos necessários

---
+++ apps/certifications/forms.py
@@ -0,0 +1,93 @@
+# apps/certifications/forms.py
+
+"""
+Definição dos formulários do aplicativo 'certifications'.
+"""
+
+from django import forms
+
+from apps.utils.widgets import BooleanSelect
+
+from .models import Certification
+
+
+class CertificationForm(forms.ModelForm):
+    """
+    Formulário para cadastro e edição de Certificações.
+    """
+
+    certifier = forms.Select(
+        label="Certificador",
+        widget=forms.Select(
+            attrs={
+                "class": "apps-form-input select2",
+                "id": "certification-certifier",
+                "placeholder": "Selecione o Certificador",
+                "autofocus": True,
+            }
+        ),
+    )
+    name = forms.CharField(
+        label="Nome da Certificação",
+        widget=forms.TextInput(
+            attrs={
+                "class": "apps-form-input",
+                "id": "certification-name",
+                "placeholder": "Digite o Nome da Certificação",
+            }
+        ),
+    )
+    examCode = forms.CharField(
+        label="Código do Exame",
+        widget=forms.TextInput(
+            attrs={
+                "class": "apps-form-input",
+                "id": "certification-examCode",
+                "placeholder": "Informe o Código do Exame",
+            }
+        ),
+    )
+    durantion = forms.IntegerField(
+        label="Duração (em minutos)",
+        widget=forms.NumberInput(
+            attrs={
+                "class": "apps-form-input",
+                "id": "certification-durantion",
+                "placeholder": "Informe a Duração em minutos",
+            }
+        ),
+        min_value=0,
+    )
+    notes = forms.CharField(
+        label="Observações desta Certificação",
+        widget=forms.Textarea(
+            attrs={
+                "class": "apps-form-input",
+                "id": "certification-notes",
+                "placeholder": "Digite uma Observação desta Certificação",
+                "rows": 4,
+            }
+        ),
+    )
+    idle = forms.BooleanField(
+        label="Certificação Inativa",
+        required=False,
+        widget=BooleanSelect(
+            attrs={
+                "class": "apps-form-input",
+                "id": "certification-idle",
+            }
+        ),
+    )
+
+    class Meta:
+        """Meta-informações para o formulário."""
+        model = Certification
+        fields = [
+            "certifier",
+            "name",
+            "examCode",
+            "durantion",
+            "notes",
+            "idle"
+        ]
