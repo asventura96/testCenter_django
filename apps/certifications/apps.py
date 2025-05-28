23b7972a3f6b6cfddbd52f1ad3909846a3723bc5
André Ventura <ti@asventura.com.br>
2025-05-28 15:43:27 -0300

    Atualiza a configuração do app 'certifications' para incluir o caminho correto e melhora a documentação.

--- apps/certifications/apps.py
+++ apps/certifications/apps.py
@@ -1,6 +1,14 @@
+# apps/certifications/apps.py
+
+"""
+Configuração do app 'certifications' para a aplicação Django.
+"""
+
 from django.apps import AppConfig
 
 
 class CertificationsConfig(AppConfig):
+    """Classe de configuração para o aplicativo 'certifications'."""
+
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'certifications'
+    name = 'apps.certifications'
