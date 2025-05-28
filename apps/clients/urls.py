12db17bdfd4c22b54856e5e1b43c3451aabe15da
André Ventura <ti@asventura.com.br>
2025-05-28 10:37:36 -0300

    Reordena as URLs de clientes para melhorar a legibilidade

--- apps/clients/urls.py
+++ apps/clients/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     # URL página Principal de Clientes
     path("clients/", views.client_home, name="client_home"),
     # URLs de Clientes
-    path("client/new/", views.client_new, name="client_new"),
     path("client/list/", views.client_list, name="client_list"),
     path("client/<int:pk>/", views.client_detail, name="client_detail"),
+    path("client/new/", views.client_new, name="client_new"),
     path("client/<int:pk>/edit/", views.client_edit, name="client_edit"),
     path("client/<int:pk>/delete/", views.client_delete, name="client_delete"),
 ]
