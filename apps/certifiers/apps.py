812368a761331c2697abd0734203f44f30e58568
André Ventura <ti@asventura.com.br>
2025-05-28 10:23:22 -0300

    Atualiza a configuração do aplicativo 'certifiers' para refletir o caminho correto e melhora a documentação

--- apps/certifiers/apps.py
+++ apps/certifiers/apps.py
@@ -1,6 +1,14 @@
+# apps/certifiers/apps.py
+
+"""
+Configuração do app 'certifiers' para a aplicação Django.
+"""
+
 from django.apps import AppConfig
 
 
 class CertifiersConfig(AppConfig):
+    """Classe de configuração para o aplicativo 'certifiers'."""
+
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'certifiers'
+    name = 'apps.certifiers'
