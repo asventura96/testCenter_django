311bd4f4ac10d9e6db7f5fa0d8ac896c6ed7c3a9
André Ventura <ti@asventura.com.br>
2025-09-02 21:56:09 -0300

    Organiza importações e garante que o Vercel encontre o projeto Django

--- api/index.py
+++ api/index.py
@@ -1,3 +1,10 @@
+import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Garante que o Vercel encontre o projeto Django
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "venturix_testCenter.settings")
 
 app = get_wsgi_application()
